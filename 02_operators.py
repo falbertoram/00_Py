499b7a686c6f35d4886dd0e6c299f5b1745beffc
Alberto Ramirez <alberto.ramirez@gmail.com>
2023-02-05 20:43:19 -0300

    init 2

---
+++ 02_operators.py
@@ -0,0 +1,53 @@
+###############
+# Operadores
+###############
+
+print(3+4)
+print(3-4)
+print(3*4)
+print(3/4)
+print(15%4) # modulo
+
+print(10 // 3) # floor division
+print(15 // 3) # floor division
+print(20 // 3) # floor division
+
+print(2 ** 3)
+
+print("Hello " + "nde tavyshos")
+
+print("Hola  " * 5)
+
+my_float = 2.5 * 3
+print("Hola  " * int(my_float))
+
+# Operadores comparativos
+
+print(3 > 4)
+print(3 < 4)
+print(3 >= 4)
+print(3 <= 4)
+print(3 == 4)
+print(3 != 4)
+
+print("**********")
+
+print("zola" < "Zola")  # Comprueba orden alfabetico por ASCII
+print("aaaa" < "abaa")
+#print("Holaaa" < "Python")
+#print("Holaaa" >= "Python")
+#print("Holaaa" <= "Python")
+#print("Holaaa" == "Python")
+#print("Holaaa" != "Python")
+
+print("**********")
+
+print((3 > 4) == False)
+
+print("**********")
+
+# Operadores logicos
+
+print(3 < 4 and "Hola" < "Python")
+print(3 < 4 or "Hola" > "Python")
+print(not (3 > 4))
